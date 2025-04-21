e30d8ec269acee100b0458edd8dd209d4e73c065
Sankarraj-Subramani <sankar.raj9@gmail.com>
2025-04-21 03:33:44 +0000

    Fixing ExtentManager

--- src/main/java/utils/ExtentManager.java
+++ src/main/java/utils/ExtentManager.java
@@ -3,11 +3,14 @@ package utils;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
+import java.io.File;
+
 public class ExtentManager {
     private static ExtentReports extent;
 
     public static ExtentReports getReportInstance() {
         if (extent == null) {
+            new File("reports").mkdirs();  // ✅ Ensure reports folder exists
             ExtentSparkReporter reporter = new ExtentSparkReporter("reports/ExtentReport.html");
             reporter.config().setReportName("AutoTestX Execution Report");
             extent = new ExtentReports();
